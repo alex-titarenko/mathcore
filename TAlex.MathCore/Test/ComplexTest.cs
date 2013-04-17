d61988c3f5ed798415b68a8ae60d61fa0f2e69ec
Alex Titarenko <alex.titarenko@talex-soft.com>
2013-04-17 23:52:39 +0300

    Unit tests for basic functions has been added.

--- TAlex.MathCore/Test/ComplexTest.cs
+++ TAlex.MathCore/Test/ComplexTest.cs
@@ -52,22 +52,6 @@ namespace TAlex.MathCore.Test
             expected.Should().Be(actual);
         }
 
-        [TestCase(-1, 0)]
-        [TestCase(2, -1)]
-        [TestCase(1, 0)]
-        [TestCase(100, 0)]
-        public void SqrtTest(double re, double im)
-        {
-            //arrange
-            Complex number = new Complex(re, im);
-            
-            //action
-            Complex sqrt = Complex.Sqrt(number);
-
-            //assert
-            NumericUtil.FuzzyEquals(sqrt * sqrt, number, 10E-10).Should().BeTrue();
-        }
-
         [TestCase(5.8, 0, 1)]
         [TestCase(-12, 0, -1)]
         [TestCase(0, 0, 0)]
@@ -87,6 +71,144 @@ namespace TAlex.MathCore.Test
             actual.Should().Be(expected);
         }
 
+        [TestCase(3, 0, 3)]
+        [TestCase(-12, 0, 12)]
+        [TestCase(0, -5, 5)]
+        [TestCase(0, 0, 0)]
+        [TestCase(12, -5, 13)]
+        public void AbsTest(double re, double im, double expected)
+        {
+            //arrange
+            Complex c = new Complex(re, im);
+
+            //action
+            double actual = Complex.Abs(c);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestCase(2, 0, 0.5, 0)]
+        [TestCase(0, 4, 0, -0.25)]
+        [TestCase(-2, 6, -0.05, -0.15)]
+        public void InverseTest(double re, double im, double reRes, double imRes)
+        {
+            //arrange
+            Complex c = new Complex(re, im);
+            Complex expected = new Complex(reRes, imRes);
+
+            //action
+            Complex actual = Complex.Inverse(c);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [Test]
+        public void InverseTest_Zero()
+        {
+            //action
+            Complex actual = Complex.Inverse(Complex.Zero);
+
+            //assert
+            double.IsNaN(actual.Re).Should().BeTrue();
+            double.IsNaN(actual.Im).Should().BeTrue();
+        }
+
+        [TestCase(3,0, 2,0, 9,0)]
+        [TestCase(2.5,0, 2.4,0, 9.01687441192008,0)]
+        [TestCase(0.1, 2.5, 0, 0, 1, 0)]
+        [TestCase(2.5, -14, -2.4, 5, -1.11492303500912, -1.43897229584281)]
+        public void PowTest(double re, double im, double reExp, double imExp, double reRes, double imRes)
+        {
+            //arrange
+            Complex input = new Complex(re, im);
+            Complex exponent = new Complex(reExp, imExp);
+            Complex expected = new Complex(reRes, imRes);
+
+            //action
+            Complex actua = Complex.Pow(input, exponent);
+
+            //assert
+            NumericUtil.FuzzyEquals(actua, expected, Machine.Epsilon);
+        }
+
+        [TestCase(-1, 0)]
+        [TestCase(2, -1)]
+        [TestCase(1, 0)]
+        [TestCase(100, 0)]
+        public void SqrtTest(double re, double im)
+        {
+            //arrange
+            Complex number = new Complex(re, im);
+            
+            //action
+            Complex sqrt = Complex.Sqrt(number);
+
+            //assert
+            NumericUtil.FuzzyEquals(sqrt * sqrt, number, Machine.Epsilon).Should().BeTrue();
+        }
+
+        [TestCase(5.2, 0, 5, 0)]
+        [TestCase(-1.1, 13.6, -2, 13)]
+        public void FloorTest(double re, double im, double reRes, double imRes)
+        {
+            //arrange
+            Complex c = new Complex(re, im);
+            Complex expected = new Complex(reRes, imRes);
+
+            //action
+            Complex actual = Complex.Floor(c);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestCase(5.2, 0, 6, 0)]
+        [TestCase(-1.1, 13.6, -1, 14)]
+        public void CeilingTest(double re, double im, double reRes, double imRes)
+        {
+            //arrange
+            Complex c = new Complex(re, im);
+            Complex expected = new Complex(reRes, imRes);
+
+            //action
+            Complex actual = Complex.Ceiling(c);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestCase(5.2, 0, 5, 0)]
+        [TestCase(-1.1, 13.6, -1, 13)]
+        public void TruncateTest(double re, double im, double reRes, double imRes)
+        {
+            //arrange
+            Complex c = new Complex(re, im);
+            Complex expected = new Complex(reRes, imRes);
+
+            //action
+            Complex actual = Complex.Truncate(c);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestCase(5.2547,0.0, 0, 5.0,0.0)]
+        [TestCase(5.56,0.0, 0, 6.0,0.0)]
+        [TestCase(346.3466,24.6732, 3, 346.347,24.673)]
+        public void RoundTest(double re, double im, int digits, double reRes, double imRes)
+        {
+            //arrange
+            Complex c = new Complex(re, im);
+            Complex expected = new Complex(reRes, imRes);
+
+            //action
+            Complex actual = Complex.Round(c, digits);
+
+            //assert
+            actual.Should().Be(expected);
+        }
 
         [Test]
         public void ArgTest_Zero()
---
+++ TAlex.MathCore/Test/ExMathTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using FluentAssertions;
+
+namespace TAlex.MathCore.Test
+{
+    [TestFixture]
+    public class ExMathTest
+    {
+        [TestCase(5.2, 0, 5, 0)]
+        [TestCase(-1.1, 13.6, -2, 13)]
+        public void IntPartTest(double re, double im, double reRes, double imRes)
+        {
+            //arrange
+            Complex c = new Complex(re, im);
+            Complex expected = new Complex(reRes, imRes);
+
+            //action
+            Complex actual = ExMath.IntPart(c);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+
+        [TestCase(5.2, 0, 0.2, 0)]
+        [TestCase(-1.1, 13.6, 0.9, 0.6)]
+        public void FracPartTest(double re, double im, double reRes, double imRes)
+        {
+            //arrange
+            Complex c = new Complex(re, im);
+            Complex expected = new Complex(reRes, imRes);
+
+            //action
+            Complex actual = ExMath.FracPart(c);
+
+            //assert
+            NumericUtil.FuzzyEquals(actual, expected, 10E-15).Should().BeTrue();
+        }
+    }
+}
