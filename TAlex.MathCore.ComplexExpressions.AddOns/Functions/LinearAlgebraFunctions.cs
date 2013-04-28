639ea76d04e4efdd0254f25e5450fbe1abe2c745
Alex Titarenko <alex.titarenko@talex-soft.com>
2013-04-28 10:21:15 +0300

    Some linear algebra functions has been added.

--- TAlex.MathCore.ComplexExpressions.AddOns/Functions/LinearAlgebraFunctions.cs
+++ TAlex.MathCore.ComplexExpressions.AddOns/Functions/LinearAlgebraFunctions.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TAlex.MathCore.ExpressionEvaluation.Trees;
+using TAlex.MathCore.ExpressionEvaluation.Trees.Metadata;
+using TAlex.MathCore.LinearAlgebra;
 
 namespace TAlex.MathCore.ExpressionEvaluation.ComplexExpressions.Functions
 {
-    class LinearAlgebraFunctions
+    [DisplayName("Matrix")]
+    [Category(Categories.LinearAlgebra)]
+    [Description("Creates new empty complex matrix with specified count of rows and columns.")]
+    [FunctionSignature("matrix", "integer rows", "integer cols")]
+    [ExampleUsage("matrix(2, 3)", "{0,0,0; 0,0,0}")]
+    public class MatrixFuncExression : BinaryExpression<Object>
     {
+        public MatrixFuncExression(Expression<Object> rowsExpression, Expression<Object> colsExpression)
+            : base(rowsExpression, colsExpression)
+        {
+        }
+
+        public override object Evaluate()
+        {
+            return new CMatrix(LeftExpression.EvaluateAsInt32(), RightExpression.EvaluateAsInt32());
+        }
     }
 }
