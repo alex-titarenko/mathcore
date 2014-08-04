8509361b434facd9670b3ffc8fb1c20e6e8e09dd
Alex Titarenko <alex.titarenko@talex-soft.com>
2014-08-05 01:48:40 +0300

    TAlex.MathCore.ComplexExpressions.Extensions library has been ported to Portable Library.

--- TAlex.MathCore.ComplexExpressions.Extensions/Properties/AssemblyInfo.cs
+++ TAlex.MathCore.ComplexExpressions.Extensions/Properties/AssemblyInfo.cs
@@ -5,22 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TAlex.MathCore.ComplexExpressions.AddOns")]
+[assembly: AssemblyTitle("TAlex.MathCore.ComplexExpressions.Extensions")]
 [assembly: AssemblyDescription("Sets of functions and constants for TAlex.MathCore.ComplexExpressions library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("T-Alex Software")]
-[assembly: AssemblyProduct("TAlex.MathCore.ComplexExpressions.AddOns")]
-[assembly: AssemblyCopyright("Copyright © 2013 T-Alex Software")]
+[assembly: AssemblyProduct("TAlex.MathCore.ComplexExpressions.Extensions")]
+[assembly: AssemblyCopyright("Copyright © 2014 T-Alex Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f92cd13e-c43c-4e90-a752-d6076d667a8c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
