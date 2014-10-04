1c8be4ef7e5d9eadd30169ef6c7ae7fc686232a4
Alex Titarenko <alex.titarenko@talex-soft.com>
2014-10-05 01:50:32 +0300

    Some quantety names have been renamed.

--- TAlex.MathCore/UnitConversion/Quantities/Area.cs
+++ TAlex.MathCore/UnitConversion/Quantities/Area.cs
@@ -8,19 +8,19 @@ using TAlex.MathCore.UnitConversion.Units;
 
 namespace TAlex.MathCore.UnitConversion.Quantities
 {
-    [Quantity("Sq. feet", "Sq. meter")]
+    [Quantity("Square feet", "Square meter")]
     public class Area : Quantity
     {
-        public static readonly LinearUnit SquareMillimeter = new LinearUnit("Sq. millimeter", "Sq. millimeters", "sq mm", 0.000001M);
-        public static readonly LinearUnit SquareCentimeter = new LinearUnit("Sq. centimeter", "Sq. centimeters", "sq cm", 0.0001M);
-        public static readonly LinearUnit SquareMeter = new LinearUnit("Sq. meter", "Sq. meters", "sq m", 1);
+        public static readonly LinearUnit SquareMillimeter = new LinearUnit("Square millimeter", "Square millimeters", "sq mm", 0.000001M);
+        public static readonly LinearUnit SquareCentimeter = new LinearUnit("Square centimeter", "Square centimeters", "sq cm", 0.0001M);
+        public static readonly LinearUnit SquareMeter = new LinearUnit("Square meter", "Square meters", "sq m", 1);
         public static readonly LinearUnit Hectare = new LinearUnit("Hectare", "Hectares", "ha", 10000);
-        public static readonly LinearUnit SquareKilometer = new LinearUnit("Sq. kilometer", "Sq. kilometers", "sq km", 1000000);
-        public static readonly LinearUnit SquareInch = new LinearUnit("Sq. inch", "Sq. inches", "sq in", 0.00064516M);
-        public static readonly LinearUnit SquareFeet = new LinearUnit("Sq. feet", "Sq. feet", "sq ft", 0.09290304M);
-        public static readonly LinearUnit SquareYard = new LinearUnit("Sq. yard", "Sq. yards", "sq yd", 0.83612736M);
+        public static readonly LinearUnit SquareKilometer = new LinearUnit("Square kilometer", "Square kilometers", "sq km", 1000000);
+        public static readonly LinearUnit SquareInch = new LinearUnit("Square inch", "Square inches", "sq in", 0.00064516M);
+        public static readonly LinearUnit SquareFeet = new LinearUnit("Square feet", "Square feet", "sq ft", 0.09290304M);
+        public static readonly LinearUnit SquareYard = new LinearUnit("Square yard", "Square yards", "sq yd", 0.83612736M);
         public static readonly LinearUnit Acre = new LinearUnit("Acre", "Acres", "ac", 4046.8564224M);
-        public static readonly LinearUnit SquareMile = new LinearUnit("Sq. mile", "Sq. miles", "sq mi", 2589988.110336M);
+        public static readonly LinearUnit SquareMile = new LinearUnit("Square mile", "Square miles", "sq mi", 2589988.110336M);
 
 
         public override string Name
--- TAlex.MathCore/UnitConversion/Quantities/Speed.cs
+++ TAlex.MathCore/UnitConversion/Quantities/Speed.cs
@@ -8,14 +8,14 @@ using TAlex.MathCore.UnitConversion.Units;
 
 namespace TAlex.MathCore.UnitConversion.Quantities
 {
-    [Quantity("Mi/H", "Km/H")]
+    [Quantity("Mile per hour", "Kilometer per hour")]
     public class Speed : Quantity
     {
-        public static readonly LinearUnit CentimeterPerSecond = new LinearUnit("Cm/S", "Centimeters per second", "cm/s", 0.01M);
-        public static readonly LinearUnit MeterPerSecond = new LinearUnit("M/S", "Meters per second", "m/s", 1);
-        public static readonly LinearUnit KilometerPerHour = new LinearUnit("Km/H", "Kilometers per hour", "km/h", 1 / 3.6M);
-        public static readonly LinearUnit FeetPerSecond = new LinearUnit("Ft/S", "Feet per second", "fps", 0.3048M);
-        public static readonly LinearUnit MilesPerHour = new LinearUnit("Mi/H", "Miles per hour", "mph", 0.44704M);
+        public static readonly LinearUnit CentimeterPerSecond = new LinearUnit("Centimeter per second", "Centimeters per second", "cm/s", 0.01M);
+        public static readonly LinearUnit MeterPerSecond = new LinearUnit("Meter per second", "Meters per second", "m/s", 1);
+        public static readonly LinearUnit KilometerPerHour = new LinearUnit("Kilometer per hour", "Kilometers per hour", "km/h", 1 / 3.6M);
+        public static readonly LinearUnit FeetPerSecond = new LinearUnit("Feet per second", "Feet per second", "fps", 0.3048M);
+        public static readonly LinearUnit MilesPerHour = new LinearUnit("Mile per hour", "Miles per hour", "mph", 0.44704M);
         public static readonly LinearUnit Knot = new LinearUnit("Knot", "Knots", "kn", 1852M / 3600);
         public static readonly LinearUnit Mach = new LinearUnit("Mach", "Mach", "M", 340.2933M);
 
